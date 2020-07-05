8d412722a96cf5a7b60a1def67b7fd96904b0498
Caroline Forrester <carolineforrester22@gmail.com>
2020-07-05 13:54:14 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,24 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-x = "X"
-o = "O"
-board = ["x", "o", "o", "x", "x", "o", "o", "o", "x"]
-def display_board(board)
- puts  " #{board[0]}  | #{board[1]}  | #{board[2]}  \n"
- puts  "-----------\n"
- puts  " #{board[3]}  | #{board[4]}  | #{board[5]}  \n"
- puts  "-----------\n"
- puts  " #{board[6]} |  #{board[7]}  | #{board[8]}  \n"
-end
-display_board(board)
+seperator = "|"
+lines = "-----------"
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-board_1 = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-def display_board(board_1)
- puts  " #{board_1[0]}| #{board_1[1]}| #{board_1[2]} \n"
- puts  "-----------\n"
- puts  " #{board_1[3]}  | #{board_1[4]}  | #{board_1[5]}  \n"
- puts  "-----------\n"
- puts  " #{board_1[6]} |  #{board_1[7]}  | #{board_1[8]}  \n"
+def display_board(board)
+  puts "#{board[0]} #{seperator}"
 end
-display_board(board_1)
