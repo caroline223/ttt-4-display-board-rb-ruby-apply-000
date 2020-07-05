28cb23a08ecfa8e3e9afc6dabf67f2aa0aa05bdf
Caroline Forrester <carolineforrester22@gmail.com>
2020-07-05 13:56:01 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,5 +6,9 @@ lines = "-----------"
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def display_board(board)
-  puts "#{board[0]} #{seperator}"
+  puts "#{board[0]} #{seperator}#{board[1]} #{seperator}#{board[2]} #{seperator}"
+  puts "#{lines}"
+  puts "#{board[3]} #{seperator}#{board[4]} #{seperator}#{board[5]} #{seperator}"
+  puts "#{lines}"
+  puts "#{board[6]} #{seperator}#{board[7]} #{seperator}#{board[8]} #{seperator}"
 end
